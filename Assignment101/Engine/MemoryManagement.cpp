6d4837d40b8693a1c775d4a22642bbfcad044007
u1148646 <utkarshrao.viswavasu@utah.edu>
2017-11-17 19:19:14 -0700

    overloading new complete

--- Assignment101/Engine/MemoryManagement.cpp
+++ Assignment101/Engine/MemoryManagement.cpp
@@ -1,2 +1,36 @@
 #pragma once
+#include<stdlib.h>
+#include "MemoryAllocator.h"
+#include<assert.h>
+enum ALIGNMENT {
+	DEFAULT,
+	ALIGN_16,
+	ALIGN_32
+};
 
+void * operator new(size_t size)
+{
+	return _aligned_malloc(size, 4);
+}
+
+void * operator new(size_t size, ALIGNMENT align)
+{
+	switch (align)
+	{
+	case DEFAULT:
+		return _aligned_malloc(size, 4);
+		break;
+	case ALIGN_16:
+		return _aligned_malloc(size, 16);
+		break;
+	case ALIGN_32:
+		return _aligned_malloc(size, 32);
+		break;
+	}
+}
+
+void * operator new(size_t size, MemoryAllocator * heap, unsigned int alignment)
+{
+	assert(heap != NULL);
+	heap->alloc(size, alignment);
+}
