28f536f4c26e2c29f8a2db4485bf2cc35384a10d
Utkarsh Rao Viswavasu <hootkarsh@gmail.com>
2017-12-12 12:45:00 -0700

    added functionality to bitarray class

--- Assignment101/Engine/BitArray.cpp
+++ Assignment101/Engine/BitArray.cpp
@@ -1,22 +1,89 @@
 #pragma once
 #include "BitArray.h"
-#include<assert.h>
+
 #include"MemoryManagement.h"
+
 
  BitArray * BitArray::Create(size_t numBits, bool initToZero, MemoryAllocator * heap)
 {
-	 assert(numBits && "bit array size can't be 0");
-	 assert(heap && "invalid heap passed");
-#ifdef _WIN32
-	 const size_t bitPerByte = 32;
-	 bits = reinterpret_cast<uint32_t *>(operator new(numBits / bitPerByte, heap));
-#elif _WIN64
-	 const size_t bitPerByte = 64;
-	 bits = reinterpret_cast<uint64_t *>(operator new(numBits / bitPerByte, heap));
-#endif
-	 
+	 return new BitArray(numBits, initToZero, heap);
+}
+
+ void BitArray::setAll(bool setValue)
+ {
 	 assert(bits);
-	 memset(bits, initToZero ? 0 : 1, numBits / bitPerByte);
+	 memset(bits, setValue ? 0 : 1, sizeOfArray / bitPerByte);
+ }
 
+void BitArray::setBit(size_t index)
+{
+	assert(index > 0);
+	size_t bitIndex = index / bitPerByte;
+	size_t offset = index % bitPerByte;
 
+#ifdef _WIN32
+	uint32_t mask = 1;
+#elif _WIN64
+	uint64_t mask = 1;
+#endif
+	mask << offset;
+	bits[index] = bits[index] | mask;
+}
+
+void BitArray::clearBit(size_t index)
+{
+	assert(index > 0);
+	size_t bitIndex = index / bitPerByte;
+	size_t offset = index % bitPerByte;
+
+#ifdef _WIN32
+	uint32_t mask = 1;
+#elif _WIN64
+	uint64_t mask = 1;
+#endif
+	mask << offset;
+	bits[index] = bits[index] - mask;
+}
+
+bool BitArray::isSet(size_t index) const
+{
+	assert(index > 0);
+	size_t bitIndex = index / bitPerByte;
+	size_t offset = index % bitPerByte;
+
+#ifdef _WIN32
+	uint32_t mask = 1;
+	uint32_t temp = bits[index];
+#elif _WIN64
+	uint64_t mask = 1;
+	uint64_t temp = bits[index];
+#endif
+	mask << offset;
+	
+	temp = temp | mask;
+	if (temp == bits[index])
+		return true;
+	else 
+		return false;
+}
+
+bool BitArray::isClear(size_t index) const
+{
+	assert(index > 0);
+	size_t bitIndex = index / bitPerByte;
+	size_t offset = index % bitPerByte;
+
+#ifdef _WIN32
+	uint32_t mask = 1;
+	uint32_t temp = bits[index];
+#elif _WIN64
+	uint64_t mask = 1;
+	uint64_t temp = bits[index];
+#endif
+	mask << offset;
+	temp = temp - mask;
+	if (temp == bits[index])
+		return true;
+	else 
+		return false;
 }
--- Assignment101/Engine/BitArray.h
+++ Assignment101/Engine/BitArray.h
@@ -3,11 +3,26 @@
 #include<cstdint>
 #include "MemoryAllocator.h"
 #include<string.h>
+#include<assert.h>
 
 class BitArray
 {
 public:
-	BitArray * Create(size_t numBits, bool initToZero, MemoryAllocator * heap);
+	BitArray(size_t numBits, bool initToZero, MemoryAllocator * heap)
+	{
+		assert(numBits && "bit array size can't be 0");
+		assert(heap && "invalid heap passed");
+#ifdef _WIN32
+		bits = reinterpret_cast<uint32_t *>(operator new(numBits / bitPerByte, heap));
+#elif _WIN64
+		bits = reinterpret_cast<uint64_t *>(operator new(numBits / bitPerByte, heap));
+#endif
+
+		assert(bits);
+		memset(bits, initToZero ? 0 : 1, numBits / bitPerByte);
+		sizeOfArray = numBits;
+	}
+	static BitArray * Create(size_t numBits, bool initToZero, MemoryAllocator * heap);
 	~BitArray();
 	size_t firstSet();
 	size_t firstClear();
@@ -15,15 +30,16 @@ public:
 	bool isSet(size_t index) const;
 	void setBit(size_t index);
 	void clearBit(size_t index);
-	void setAll();
-	void clearAll();
-
+	void setAll(bool setValue);
+	bool areAllClear();
 private:
 #ifdef _WIN32
 		uint32_t * bits;
+		const size_t bitPerByte = 32;
 #elif _WIN64
 		uint64_t * bits;
+		const size_t bitPerByte = 64;
 #endif
-
+		size_t sizeOfArray;
 
 };
